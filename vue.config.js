d34d2011f8ac5d799703ac3dddb81b9df7362a72
Rael-x <adebolaakomolafe@gmail.com>
2021-12-28 21:09:19 +0100

    installed dotenv and resolved config issue

--- vue.config.js
+++ vue.config.js
@@ -9,15 +9,10 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].template = "./index.html";
+      return args;
+    });
+  },
 };
-
-module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].template = './index.html'
-        return args
-      })
-  }
-}
